c68306453481e96f0ced9e80f3f11f75f9d4ae57
AMAN <nottherealaman@gmail.com>
2022-12-13 23:57:24 +0530

    add image

--- 5miles/App.js
+++ 5miles/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View  , Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Image source={require('./assets/5miles-logo.png')} style={{ width: 40, height: 40 }} />
+
       <StatusBar style="auto" />
     </View>
   )
